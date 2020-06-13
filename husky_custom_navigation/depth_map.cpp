b75b08aeaf28254d876abbb3d807ec70c211f816
stellarator-x <admathur@iitk.ac.in>
2020-06-14 02:00:18 +0530

    adding depth_camera

---
+++ husky_custom_navigation/depth_map.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <stereo_msgs/DisparityImage.h>
+
+
+using namespace std;
+
+float get_depth_image()
